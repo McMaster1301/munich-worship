c804ee6ad1ad900a8fd768a9a62cb1dec15101eb
16flotho <tom.florian@hotmail.com>
2024-03-21 09:20:44 +0100

    feat: change leaderboard

--- lib/leaderboards.dart
+++ lib/leaderboards.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 
 class LeaderboardEntry {
   final String name;
@@ -11,50 +12,58 @@ class LeaderboardEntry {
 
 class LeaderboardEntryWidget extends StatelessWidget {
   final LeaderboardEntry entry;
-  final int place;
+  final int? place;
 
   LeaderboardEntryWidget(this.entry, this.place);
 
+  Color determineColor(int? place, int accent) {
+    if (place == null) {
+      return Colors.white;
+    } else
+    if (place == 1) {
+      return Colors.deepPurple[300]!;
+    } else if (place == 2) {
+      return Colors.deepPurple[200]!;
+    } else if (place == 3) {
+      return Colors.deepPurple[100]!;
+    } else {
+      return Colors.white;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Color? placeColor;
-    if (place == 1) {
-      placeColor = Colors.amber[200];
-    } else if (place == 2) {
-      placeColor = Colors.grey[200];
-    } else if (place == 3) {
-      placeColor = Colors.brown[200];
-    } else {
-      placeColor = Colors.white;
-    }
-
     return Card(
-      color: placeColor,
+      color: determineColor(place, 100),
       child: ListTile(
-        leading: Text(
-          "$place.",
-          style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
-          ),
-        ),
         title: Row(
           children: [
+            Text(
+              place != null ? place.toString() : "--",
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(width: 8.0),
             CircleAvatar(
               backgroundImage: NetworkImage(entry.profilePictureUrl),
             ),
             SizedBox(width: 8.0),
             Text(
               entry.name,
-              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
             ),
+            Spacer(),
+            Text(
+              entry.score.toString(),
+              // style: TextStyle(color: Colors.grey[600]),
+            )
           ],
         ),
-        trailing: Text(
-          entry.score.toString(),
-          style: TextStyle(color: Colors.grey[600]),
-        ),
+        // trailing: T,
       ),
     );
   }
@@ -66,7 +75,26 @@ class Leaderboards extends StatefulWidget {
   State<Leaderboards> createState() => _LeaderboardsState();
 }
 
+List<LeaderboardEntry> generateUsers(int numberOfUsers) {
+  List<LeaderboardEntry> users = [];
+
+  for (int i = 1; i <= numberOfUsers; i++) {
+    String name = 'User$i';
+    String profilePictureUrl =
+        'https://xsgames.co/randomusers/assets/avatars/male/$i.jpg';
+    int score = 100 * (5 - i);
+
+    users.add(LeaderboardEntry(name, profilePictureUrl, score));
+  }
+
+  return users;
+}
+
 class _LeaderboardsState extends State<Leaderboards> {
+  List<LeaderboardEntry> users = generateUsers(15);
+  LeaderboardEntry currentUser = LeaderboardEntry('Current User',
+      'https://xsgames.co/randomusers/assets/avatars/male/6.jpg', 80);
+
   @override
   void initState() {
     super.initState();
@@ -81,12 +109,23 @@ class _LeaderboardsState extends State<Leaderboards> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListView.builder(
-                itemCount: 5,
+                itemCount: users.length,
                 itemBuilder: (_, index) {
-                  final entry = LeaderboardEntry('name', 'https://picsum.photos/250?image=9', 100);
+                  final entry = users[index];
                   return LeaderboardEntryWidget(entry, index + 1);
                 }),
           ),
+        ),
+        Container(
+          // decoration: BoxDecoration(
+          //   color: Colors.grey[100],
+          //   borderRadius: BorderRadius.only(
+          //     topRight: Radius.circular(10),
+          //     topLeft: Radius.circular(10),
+          //   ),
+          // )
+          alignment: Alignment.center,
+          child: LeaderboardEntryWidget(currentUser, null),
         ),
       ],
     );
