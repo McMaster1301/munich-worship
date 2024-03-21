221b0356da146e0cfd19332428db4b8e00aff76e
16flotho <tom.florian@hotmail.com>
2024-03-21 02:04:20 +0100

    feat: add leaderboards

--- lib/leaderboards.dart
+++ lib/leaderboards.dart
@@ -1,4 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LeaderboardEntry {
+  final String name;
+  final String profilePictureUrl;
+  final int score;
+
+  LeaderboardEntry(this.name, this.profilePictureUrl, this.score);
+}
+
+class LeaderboardEntryWidget extends StatelessWidget {
+  final LeaderboardEntry entry;
+  final int place;
+
+  LeaderboardEntryWidget(this.entry, this.place);
+
+  @override
+  Widget build(BuildContext context) {
+    Color? placeColor;
+    if (place == 1) {
+      placeColor = Colors.amber[200];
+    } else if (place == 2) {
+      placeColor = Colors.grey[200];
+    } else if (place == 3) {
+      placeColor = Colors.brown[200];
+    } else {
+      placeColor = Colors.white;
+    }
+
+    return Card(
+      color: placeColor,
+      child: ListTile(
+        leading: Text(
+          "$place.",
+          style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+          ),
+        ),
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(entry.profilePictureUrl),
+            ),
+            SizedBox(width: 8.0),
+            Text(
+              entry.name,
+              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        trailing: Text(
+          entry.score.toString(),
+          style: TextStyle(color: Colors.grey[600]),
+        ),
+      ),
+    );
+  }
+}
 
 class Leaderboards extends StatefulWidget {
   @override
@@ -15,9 +75,20 @@ class _LeaderboardsState extends State<Leaderboards> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        decoration: BoxDecoration(border: Border.all()),
-        child: const Text(overflow: TextOverflow.ellipsis, 'Leaderboards'));
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (_, index) {
+                  final entry = LeaderboardEntry('name', 'https://picsum.photos/250?image=9', 100);
+                  return LeaderboardEntryWidget(entry, index + 1);
+                }),
+          ),
+        ),
+      ],
+    );
   }
 }
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/finn/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/finn/Documents/24StartHackMunichWorship/munich_worship"
+export "FLUTTER_ROOT=/Users/thomasflorian/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thomasflorian/StudioProjects/munic_worship"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
