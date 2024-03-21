d6517cbf0852fa671be5d303b1a07e9a67422cba
McMaster1301 <finn.droege13@gmail.com>
2024-03-21 01:09:22 +0100

    feat: basic cards

--- lib/events.dart
+++ lib/events.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+var events = [1, 2, 3];
+
 class Events extends StatefulWidget {
   @override
   // ignore: no_logic_in_create_state
@@ -13,11 +15,88 @@ class _EventsState extends State<Events> {
     //This is the place to fetch sth for api since it is only done once (not in build where it is rebuilt every time)
   }
 
+  Widget buildListElement(context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        decoration: BoxDecoration(border: Border.all()),
-        child: const Text(overflow: TextOverflow.ellipsis, 'Events'));
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                itemCount: events.length,
+                itemBuilder: (_, index) {
+                  final event = events[index];
+                  return buildListElement(context);
+                }),
+          ),
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.star,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+                label: const Text(
+                  "I'm feeling lucky!",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.local_shipping,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+                label: const Text(
+                  "Order",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
